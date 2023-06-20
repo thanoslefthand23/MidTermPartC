01fb8a242ed21ea37256ac1acd58a51c0dc6fd02
thanoslefthand23 <133786464+thanoslefthand23@users.noreply.github.com>
2023-06-20 10:11:20 -0400

    Update UserProfile.java

--- src/midtermexam_versiona_extensioncode/UserProfile.java
+++ src/midtermexam_versiona_extensioncode/UserProfile.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * Comment is added to the userprofile file for the midterm exam
  */
 package midtermexam_versiona_extensioncode;
 
